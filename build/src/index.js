3309c3e34fbc8311db2c12dfe62e2f1bc38da228
yunyuseok <sayarn960@naver.com>
2022-02-11 19:09:31 +0900

    quiz

--- build/src/index.js
+++ build/src/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 require('dotenv').config();
+const quiz = require("./data/quiz.json");
 const intents = new discord_js_1.Intents();
 intents.add(discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES);
 const client = new discord_js_1.Client({ intents });
@@ -9,7 +10,6 @@ client.once('ready', async () => {
     var _a;
     console.log(`${(_a = client.user) === null || _a === void 0 ? void 0 : _a.tag}에 로그인하였습니다!`);
 });
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 client.on('message', msg => {
     var _a;
     if (msg.author.bot)
@@ -17,7 +17,22 @@ client.on('message', msg => {
     if (msg.author.id === ((_a = client.user) === null || _a === void 0 ? void 0 : _a.id))
         return; // 봇으로 인한 접속 금지
     if (msg.content === '!퀴즈') {
-        msg.channel.send(`퀴즈 봇이 대답할거임`);
+        const limit = 5;
+        const item = quiz[Math.floor(Math.random() * quiz.length)];
+        const filter = (res) => {
+            return item.answer.some((answer) => answer === res.content);
+        };
+        msg.channel.send(`${item.question} (제한시간 : ${limit})`)
+            .then(() => {
+            msg.channel.awaitMessages({ filter, max: 1, time: limit * 1000, errors: ["time"] })
+                .then((collected) => {
+                var _a;
+                msg.channel.send(`${(_a = collected.first()) === null || _a === void 0 ? void 0 : _a.author} <- 정답!`);
+            })
+                .catch((err) => {
+                msg.channel.send("제한시간이 지났습니다.");
+            });
+        });
     }
 });
 client.login(process.env.token);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import {Client, Intents} from 'discord.js';
+import {Client, Intents, Message} from 'discord.js';
 require('dotenv').config();
+
+import quiz = require('./data/quiz.json');
 
 const intents = new Intents();
 intents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
@@ -10,13 +12,28 @@ client.once('ready', async () => {
   console.log(`${client.user?.tag}에 로그인하였습니다!`);
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 client.on('message', msg => {
   if (msg.author.bot) return; // 무한 방지
   if (msg.author.id === client.user?.id) return; // 봇으로 인한 접속 금지
 
   if (msg.content === '!퀴즈') {
-    msg.channel.send(`퀴즈 봇이 대답할거임`);
+    const limit = 5;
+    const item = quiz[Math.floor(Math.random() * quiz.length)];
+  
+    const filter = (res : Message) => {
+      return item.answer.some((answer : string) => answer === res.content);
+    };
+    
+    msg.channel.send(`${item.question} (제한시간 : ${limit})`)
+    .then(() => {
+      msg.channel.awaitMessages({filter, max: 1, time: limit * 1000, errors: ["time"]})
+      .then((collected) => {
+        msg.channel.send(`${collected.first()?.author} <- 정답!`);
+      })
+      .catch((err) => {
+        msg.channel.send("제한시간이 지났습니다.");
+      })
+    })
   }
 });
 
